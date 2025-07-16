e41b213cff6f21e958f559f783339bda9e5a98a8
nitin <nitinvishwakarma9138@gmail.com>
2025-07-16 12:05:23 +0530

    Implement admin access control, enhance error handling, and redesign 404 page with interactive game and improved styles

--- OneDrive/Documents/Desktop/AI-Diary/app.js
+++ OneDrive/Documents/Desktop/AI-Diary/app.js
@@ -3,7 +3,9 @@ const app = express();
 // const mongoose=require("mongoose")
 // const methodOverride = require("method-override")
 const path = require("path");
-const ejsMate=require("ejs-mate")
+const ejsMate=require("ejs-mate");
+const wrapAsync  = require ("./utils/wrapAsync.js");
+const ExpressError  = require ("./utils/ExpressError.js");
 
 // const mongo_url ="mongodb://127.0.0.1:27017/";
 // main().then((res)=>{
@@ -21,6 +23,24 @@ app.set("views", path.join(__dirname,"views"))
 // app.use(methodOverride("_method"))
 app.engine("ejs",ejsMate)
 app.use(express.static(path.join(__dirname, "public")));
+
+app.use("/admin",(req,res,next)=>{
+   let {token}=req.query;
+   if(token==="eshin"){
+    next()}
+    throw new ExpressError(403,"Access for admin page is forbidden ")
+    
+})
+
+app.use((err, req, res, next) => {
+  let {status=500,message="something error"}=err;
+  res.status(status).send(message);
+  next(err);
+})
+
+app.get("/admin",(req,res,next)=>{
+    res.send("data")
+})
 
 app.get("/",(req,res)=>{
    res.render("diary/landing.ejs")
@@ -61,8 +81,14 @@ app.get("/AI-diary/entry-details",(req,res)=>{
 
 
 app.use((req,res,next)=>{
-res.status(404).render("diary/404.ejs")
-    next()
+next(new ExpressError(404,"page not found"))
+
+})
+
+app.use((err,req,res,next)=>{
+let {statusCode=500,message="something went wrong"}=err;
+ res.status(statusCode).render("diary/404.ejs");
+// res.status(statusCode).send(message);
 })
 
 app.listen(5000,()=>{
